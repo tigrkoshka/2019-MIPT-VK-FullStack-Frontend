ce5291de14df307ff4452ba2ca08a8d7983fbb6e
Tigran <tigrankoshkelyan@gmail.com>
2020-03-22 22:42:18 +0300

    Tests fixed

--- endtoend/jest.config.js
+++ endtoend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-puppeteer',
   testRegex: './*\\.test\\.js$',
-  testTimeout: 50000,
+  testTimeout: 70000,
 }
--- endtoend/sendMessage.test.js
+++ endtoend/sendMessage.test.js
@@ -8,14 +8,14 @@ describe('Send message test', () => {
   it('should send message without error', async () => {
     await expect(page).toFill('[placeholder="Enter your tag"]', '@Tigran')
     await expect(page).toFill('[placeholder="Enter your password"]', '@Tigran')
-    await expect(page).toClick('[class="welcomePageStyles_content__3EfBu"]')
+    await expect(page).toClick('[class^="welcomePageStyles_content"]')
     await page.waitForSelector('[href="#/MessageForm/@group_1/Group started by Tigran/1"]')
     await expect(page).toClick('[href="#/MessageForm/@group_1/Group started by Tigran/1"]')
     await expect(page).toFill('[placeholder="Your message"]', 'Hello test')
     await page.keyboard.press('Enter')
-    await expect(page).toClick('[class="headerStyles_text__3zlfI"]')
+    await expect(page).toClick('[class^="headerStyles_text"]')
 
     await page.waitFor(1000)
-    await expect(page).toClick('[class="headerStyles_text__3zlfI"]')
+    await expect(page).toClick('[class^="headerStyles_text"]')
   })
 })
