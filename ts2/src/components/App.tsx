affc93e3ea7450622429a452d348a6a63369fc0c
Tigran <tigrankoshkelyan@gmail.com>
2020-04-21 01:36:53 +0300

    D3 visualization added

---
+++ ts2/src/components/App.tsx
@@ -0,0 +1,144 @@
+import * as d3 from 'd3'
+import _ from 'lodash'
+import React from 'react'
+
+import initData from '../static/data.json'
+import { Axis, AxisCalc, D3Short, DailyRecord, GraphData, Margin, Size, UsefulData } from '../types/types'
+
+function sortByDate(a: DailyRecord, b: DailyRecord): number {
+  return +a.graph.date - +b.graph.date
+}
+
+function manageData(): UsefulData {
+  const usedData: DailyRecord[] = initData.map(
+    ({ dateChecked, positive, state }): DailyRecord => ({
+      graph: {
+        date: new Date(dateChecked),
+        value: positive,
+      },
+      state,
+    }),
+  )
+
+  usedData.sort(sortByDate)
+  const dateRange: Date[] = [usedData[0].graph.date, usedData[usedData.length - 1].graph.date]
+  const maxCases: number = d3.max(usedData, (d: DailyRecord): number => d.graph.value)!
+
+  const extendedData: { [x: string]: DailyRecord[] } = _.groupBy<DailyRecord>(
+    usedData,
+    (r: DailyRecord): string => r.state,
+  )
+
+  const data: { [x: string]: GraphData[] } = {}
+  for (const key in extendedData) {
+    if (extendedData.hasOwnProperty(key)) {
+      data[key] = extendedData[key].map((gr: DailyRecord): GraphData => gr.graph)
+    }
+  }
+
+  return { dateRange, data, maxCases }
+}
+
+function getAxisCalculations(dateRange: Date[], maxCases: number, size: Size, margin: Margin): AxisCalc {
+  const x: d3.ScaleTime<number, number> = d3
+    .scaleUtc()
+    .domain(dateRange)
+    .range([margin.left, size.width - margin.right])
+
+  const y: d3.ScaleLinear<number, number> = d3
+    .scaleLinear()
+    .domain([0, maxCases])
+    .nice()
+    .range([size.height - margin.bottom, margin.top])
+
+  return { xCalc: x, yCalc: y }
+}
+
+function getAxis(calc: AxisCalc, size: Size, margin: Margin): Axis {
+  const xAxis: (g: any) => D3Short = (g: any): D3Short =>
+    g.attr('transform', `translate(0,${size.height - margin.bottom})`).call(
+      d3
+        .axisBottom(calc.xCalc)
+        .ticks(size.width / 100)
+        .tickSizeOuter(0),
+    )
+
+  const yAxis: (g: any) => D3Short = (g: any): D3Short =>
+    g
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(
+        d3
+          .axisLeft(calc.yCalc)
+          .ticks(size.width / 100)
+          .tickSizeOuter(0),
+      )
+      .call((g: D3Short): D3Short => g.select('.domain').remove())
+      .call(
+        (g: D3Short): D3Short =>
+          g
+            .select('.tick:last-of-type text')
+            .clone()
+            .attr('x', 3)
+            .attr('text-anchor', 'start')
+            .attr('font-weight', 'bold')
+            .attr('font-size', '16px')
+            .text('Number of positive Covid-19 tests in the US by states'),
+      )
+
+  return { xAxis, yAxis }
+}
+
+class App extends React.Component {
+  margin: Margin = { top: 20, right: 30, bottom: 30, left: 50 }
+  ref!: SVGSVGElement
+  windowSize: Size = { height: document.documentElement.clientHeight, width: document.documentElement.clientWidth }
+
+  componentDidMount(): void {
+    const { dateRange, data, maxCases }: UsefulData = manageData()
+
+    const { xCalc, yCalc }: AxisCalc = getAxisCalculations(dateRange, maxCases, this.windowSize, this.margin)
+
+    const { xAxis, yAxis }: Axis = getAxis({ xCalc, yCalc }, this.windowSize, this.margin)
+
+    const line: d3.Line<GraphData> = d3
+      .line<GraphData>()
+      .defined((d: GraphData): boolean => d !== undefined)
+      .x((d: GraphData): number => xCalc(d.date))
+      .y((d: GraphData): number => yCalc(d.value))
+
+    const svg: d3.Selection<SVGSVGElement, unknown, null, undefined> = d3
+      .select(this.ref)
+      .attr('viewBox', `0, 0, ${this.windowSize.width}, ${this.windowSize.height}`)
+
+    svg.append('g').call(xAxis)
+
+    svg.append('g').call(yAxis)
+
+    for (const key in data) {
+      if (data.hasOwnProperty(key) && data[key].length > 45) {
+        svg
+          .append('path')
+          .datum(data[key])
+          .attr('fill', 'none')
+          .attr('stroke', 'black')
+          .attr('stroke-width', 1.5)
+          .attr('stroke-linejoin', 'round')
+          .attr('stroke-linecap', 'round')
+          .attr('d', line)
+      }
+    }
+  }
+
+  render(): any {
+    return (
+      <svg
+        className="container"
+        ref={(ref: SVGSVGElement): SVGSVGElement => (this.ref = ref)}
+        height="100%"
+        width="100%"
+      />
+    )
+  }
+}
+
+export default App
---
+++ ts2/src/types/types.ts
@@ -0,0 +1,41 @@
+import * as d3 from 'd3'
+
+export type Margin = {
+  top: number
+  right: number
+  bottom: number
+  left: number
+}
+
+export type GraphData = {
+  date: Date
+  value: number
+}
+
+export type DailyRecord = {
+  graph: GraphData
+  state: string
+}
+
+export type UsefulData = {
+  dateRange: Date[]
+  data: { [key: string]: GraphData[] }
+  maxCases: number
+}
+
+export type AxisCalc = {
+  xCalc: d3.ScaleTime<number, number>
+  yCalc: d3.ScaleLinear<number, number>
+}
+
+export type Axis = {
+  xAxis: (g: any) => any
+  yAxis: (g: any) => any
+}
+
+export type Size = {
+  height: number
+  width: number
+}
+
+export type D3Short = d3.Selection<d3.BaseType, any, d3.BaseType, any>
